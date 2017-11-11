2f2a9361ad7bae4e74322f7e69a204a04c226ea1
Sam Rang <srang2010@gmail.com>
2017-11-11 14:30:35 -0600

    gif

--- src/main/resources/static/js/lab.js
+++ src/main/resources/static/js/lab.js
@@ -1,32 +1,36 @@
-var yoyo     = new Audio('audio/yoyo.wav');
-var bass     = new Audio('audio/bass.wav');
-var cha      = new Audio('audio/cha.wav');
-var shaker   = new Audio('audio/shaker.wav');
-var conch    = new Audio('audio/conch.wav');
-var snare    = new Audio('audio/snare.wav');
-var snare2   = new Audio('audio/snare2.wav');
-var yoyobass = new Audio('audio/yoyobass.wav');
-$('.yoyo').click(function() {
-    yoyo.play()
-});
-$('.bass').click(function() {
-    bass.play()
-});
+var cha = new Audio('audio/cha.wav');
 $('.cha').click(function() {
-    cha.play()
+    cha.play();
+    $('#vid').html('<video width="320" height="240" src="/img/cha.mov" autoplay></video>');
 });
+var concha = new Audio('audio/concha1.wav');
+$('.concha').click(function() {
+    concha.play();
+    $('#vid').html('<video width="320" height="240" src="/img/con_.mov" autoplay></video>');
+});
+var kick = new Audio('audio/kick.wav');
+$('.kick').click(function() {
+    kick.play();
+    $('#vid').html('<video width="320" height="240" src="/img/kick.mov" autoplay></video>');
+});
+var shaker = new Audio('audio/shaker.wav');
 $('.shaker').click(function() {
-    shaker.play()
+    shaker.play();
+    $('#vid').html('<video width="320" height="240" src="/img/shaker.mov" autoplay></video>');
 });
-$('.conch').click(function() {
-    conch.play()
-});
+var snare = new Audio('audio/snare.wav');
 $('.snare').click(function() {
-    snare.play()
+    snare.play();
+    $('#vid').html('<video width="320" height="240" src="/img/snare.mov" autoplay></video>');
 });
-$('.snare2').click(function() {
-    snare2.play()
+var drum = new Audio('audio/snare2.wav');
+$('.drum').click(function() {
+    drum.play();
+    $('#vid').html('<video width="320" height="240" src="/img/rattle.mov" autoplay></video>');
+
 });
+var yoyobass = new Audio('audio/yoyobass.wav');
 $('.yoyobass').click(function() {
-    yoyobass.play()
+    yoyobass.play();
+    $('#vid').html('<video width="320" height="240" src="/img/bass.mov" autoplay></video>');
 });
