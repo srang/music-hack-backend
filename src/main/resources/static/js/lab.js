ed783eb79a74b9e58ec9dd30bd26eed7bd560f55
Sam Rang <srang2010@gmail.com>
2017-11-11 15:16:49 -0600

    videos as buttons

--- src/main/resources/static/js/lab.js
+++ src/main/resources/static/js/lab.js
@@ -1,36 +1,43 @@
 var cha = new Audio('audio/cha.wav');
 $('.cha').click(function() {
     cha.play();
-    $('#vid').html('<video width="320" height="240" src="/img/cha.mov" autoplay></video>');
+    $('.cha video')[0].play();
+    // $('#vid').html('<video width="320" height="240" src="/img/cha.mov" autoplay></video>');
 });
 var concha = new Audio('audio/concha1.wav');
 $('.concha').click(function() {
     concha.play();
-    $('#vid').html('<video width="320" height="240" src="/img/con_.mov" autoplay></video>');
+    $('.concha video')[0].play();
+    // $('#vid').html('<video width="320" height="240" src="/img/concha.mov" autoplay></video>');
 });
 var kick = new Audio('audio/kick.wav');
 $('.kick').click(function() {
     kick.play();
-    $('#vid').html('<video width="320" height="240" src="/img/kick.mov" autoplay></video>');
+    $('.kick video')[0].play();
+    // $('#vid').html('<video width="320" height="240" src="/img/kick.mov" autoplay></video>');
 });
 var shaker = new Audio('audio/shaker.wav');
 $('.shaker').click(function() {
     shaker.play();
-    $('#vid').html('<video width="320" height="240" src="/img/shaker.mov" autoplay></video>');
+    $('.shaker video')[0].play();
+    // $('#vid').html('<video width="320" height="240" src="/img/shaker.mov" autoplay></video>');
 });
-var snare = new Audio('audio/snare.wav');
-$('.snare').click(function() {
+var snare = new Audio('audio/snare1.wav');
+$('.snare1').click(function() {
     snare.play();
-    $('#vid').html('<video width="320" height="240" src="/img/snare.mov" autoplay></video>');
+    $('.snare1 video')[0].play();
+    // $('#vid').html('<video width="320" height="240" src="/img/snare.mov" autoplay></video>');
 });
-var drum = new Audio('audio/snare2.wav');
-$('.drum').click(function() {
-    drum.play();
-    $('#vid').html('<video width="320" height="240" src="/img/rattle.mov" autoplay></video>');
+var snare2 = new Audio('audio/snare2.wav');
+$('.snare2').click(function() {
+    snare2.play();
+    $('.snare2 video')[0].play();
+    // $('#vid').html('<video width="320" height="240" src="/img/rattle.mov" autoplay></video>');
 
 });
 var yoyobass = new Audio('audio/yoyobass.wav');
 $('.yoyobass').click(function() {
     yoyobass.play();
-    $('#vid').html('<video width="320" height="240" src="/img/bass.mov" autoplay></video>');
+    $('.yoyobass video')[0].play();
+    // $('#vid').html('<video width="320" height="240" src="/img/bass.mov" autoplay></video>');
 });
